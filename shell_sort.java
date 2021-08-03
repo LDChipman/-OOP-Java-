6d9e5d350dd6162bf60a4f389a98ada564d47b05
Liam Chipman <chipman.liam@gmail.com>
2021-08-03 15:16:26 -0300

    Removed main method, added comments, and cleaned up code

--- shell_sort.java
+++ shell_sort.java
@@ -1,61 +1,70 @@
-import java.util.Arrays;
-
 public class shell_sort {
-    
-    public static void main(String[] args) {
 
-        System.out.println("Shell Sort Demo:");
-        int[] unsorted_array = {6, 2, 9, 4, 7, 3, 1, 5, 8, 0};
-        System.out.println("Unsorted Array = " + Arrays.toString(unsorted_array));
-        int[] sorted_array = sort(unsorted_array, 0);
-        System.out.println("Sorted Array = " + Arrays.toString(sorted_array));
-        
-        
-    }
+    //Sorts an array of ints using a shell sort algorithm
+    public static int[] sort(int[] array){
 
-    public static int[] sort(int[] array, int i){
-
+        //sets the starting gap length and begins the program
         int gap = array.length / 2;
-        return compare(gap, i, i+gap, array, i);
+        return compare(gap, 0, 0+gap, array, 0);
 
     }
 
+    //Compares two numbers and then returns an array with two numbers swapped putting the lower number at the first index
     private static int[] compare(int gap, int first_num_index, int second_num_index, int[] array, int original_index){
 
+        //Checks if the gap has been reduced to 0 and if so returns the sorted array
+        if (gap == 0){
+            return array;
+        }
+
+        //Checks if the index is less than 0 and sets it to original index (needed to reset the index after swapping multiple numbers)
         if (first_num_index < 0){
 
             return compare(gap, original_index+1, original_index+1+gap, array, original_index+1);
 
-        } else if (second_num_index > array.length-1) {
+        } 
+        
+        //Checks if the second numbers index is out of bounds (lets the program know to start over at a new gap)
+        else if (second_num_index > array.length-1) {
 
             return compare(gap/2, 0, 0+gap/2, array, 0);
 
         }
 
+        /* Checks if the number at the first index is greater than the one at the second index
+           and if it is it swaps the numbers and checks if it needs to swap the number now at 
+           the first index with the one gap length before it
+        */
         if (array[first_num_index] > array[second_num_index]) {
             
             array = swap(array, first_num_index, second_num_index);
             return compare(gap, first_num_index - gap, first_num_index, array, original_index);
 
         }
-        
-        if (gap == 0){
-            return array;
-        }
 
+        //Checks to make sure the second index isn't out of bounds and then runs another comparison at the next highest index
         if (second_num_index < array.length-1){
             return compare(gap, first_num_index+1, second_num_index+1, array, original_index+1);
         }
 
+        //Compares the numbers again at the next gap starting at index 0 in the array
         return compare(gap/2, 0, 0+gap/2, array, 0);
 
     }
 
+    //Swaps the numbers at 2 indeces and returns the new array
     private static int[] swap(int[] array, int first_num_index, int second_num_index){
 
+        //Creates temporary storage for the int at the first index
         int temp = array[first_num_index];
+
+        //Sets the int at the first index to the int at the second index
         array[first_num_index] = array[second_num_index];
+
+        //Sets the int at the second index to the int at the first index by using the temporary storage of the int at the first index
         array[second_num_index] = temp;
+
+        //Returns the array with the given numbers swapped
         return array;
 
     }
